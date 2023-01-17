249eee3f211ec555f27dcde58b4c1de7b219f1e2
gabicilfone <cilfonegabriel@gmail.com>
2023-01-17 18:30:57 -0300

    add calculate js and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import Calculator from './components/Calculator';
-import './App.css';
 
-function App() {
-  return (
-    Calculator()
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
