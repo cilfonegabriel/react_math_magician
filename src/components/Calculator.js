7892b4f81ce117bce58ee1f56134e6c8be9da524
gabicilfone <cilfonegabriel@gmail.com>
2023-01-18 14:45:29 -0300

    add handleClick and add onClick  in the event

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,6 +5,11 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
@@ -12,25 +17,25 @@ class Calculator extends React.Component {
       <section className="container-calculator">
         <h3 className="result">0</h3>
         <div className="buttons-calculator">
-          <button type="button" className="btn-calculator">AC</button>
-          <button type="button" className="btn-calculator">+/-</button>
-          <button type="button" className="btn-calculator">%</button>
-          <button type="button" className="btn-calculator color-right">÷</button>
-          <button type="button" className="btn-calculator">7</button>
-          <button type="button" className="btn-calculator">8</button>
-          <button type="button" className="btn-calculator">9</button>
-          <button type="button" className="btn-calculator color-right">x</button>
-          <button type="button" className="btn-calculator">4</button>
-          <button type="button" className="btn-calculator">5</button>
-          <button type="button" className="btn-calculator">6</button>
-          <button type="button" className="btn-calculator color-right">-</button>
-          <button type="button" className="btn-calculator">1</button>
-          <button type="button" className="btn-calculator">2</button>
-          <button type="button" className="btn-calculator">3</button>
-          <button type="button" className="btn-calculator color-right">+</button>
-          <button type="button" className="btn-calculator zero">0</button>
-          <button type="button" className="btn-calculator">.</button>
-          <button type="button" className="btn-calculator color-right">=</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">AC</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">+/-</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">%</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator color-right">÷</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">7</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">8</button>
+          <button  onClick={this.handleClick} type="button" className="btn-calculator">9</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator color-right">x</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">4</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">5</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">6</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator color-right">-</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">1</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">2</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">3</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator color-right">+</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator zero">0</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator">.</button>
+          <button onClick={this.handleClick} type="button" className="btn-calculator color-right">=</button>
         </div>
       </section>
     );
