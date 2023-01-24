fa27ade8585a1aeeb7dcde69ded0d35b3c0f715e
gabicilfone <cilfonegabriel@gmail.com>
2023-01-24 16:04:52 -0300

    add Home and Quote pages, also add style

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+const Quote = () => (
+  <div className="home-section">
+    <h4 className="sentence">
+      &quot;Since you are not experienced in things of the world
+      <br />
+      all things that have some difficulty seem impossible to you.
+      <br />
+      Trust in time, which usually gives sweet solutions to many bitter difficulties&quot;
+    </h4>
+    <p className="author"> - DON QUIJOTE </p>
+  </div>
+);
+
+export default Quote;
